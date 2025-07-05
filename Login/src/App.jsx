f08ca406f3d8a0fcd87b7fe82d42150740303074
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2025-07-05 15:08:06 +0530

    add env link

--- Login/src/App.jsx
+++ Login/src/App.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://sqlauth1.onrender.com',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 })
 
 function App() {
